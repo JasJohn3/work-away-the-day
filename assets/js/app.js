cae8b12332675ada99e023c71cc5ab73eeed881f
Your Name <jasjohn3@uat.edu>
2021-07-18 04:04:30 -0700

    Initial UI setup using for loop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,31 @@
 let currentDate = document.querySelector('#currentDay');
+let currentDay = moment().format('MMMM Do YYYY');
 
 function drawDate(){
-  setInterval(()=>{
-    currentDate.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
-  }, 1000);
-  
+  currentDate.textContent = moment().format('MMMM Do YYYY');
+  // setInterval(()=>{
+    
+  // }, 1000); 
+}
+function drawTime(){
+  let timeArr =Array.from(document.querySelectorAll('#time'));
+  let hour = 9;
+  for (i = 0; i < timeArr.length; i++) {
+    const element = timeArr[i];
+    if(hour < 12 && hour >= 9){
+      element.textContent = hour +' am'
+    }else if (hour === 12) {
+      element.textContent = hour +' pm'
+    }else if(hour > 12){
+      hour = 1;
+      element.textContent = hour +' pm'
+    }else if(hour < 9){
+      element.textContent = hour +' pm'
+    }
+    hour+=1;
+  }
 }
 $(document).ready(function(){
 drawDate();
+drawTime();
 });
